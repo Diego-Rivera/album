54967e9b68f874f34f7753d9497e18ba55f824de
Diego-Rivera <diego.rivera.ramirez@gmail.com>
2022-06-07 11:24:20 -0400

    (add) modelo Foto, template, urls, app front

--- album/urls.py
+++ album/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from front.views import Index
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Index),
 ]
